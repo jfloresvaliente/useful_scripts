44fefa33a4b56b17867c704ad6f6c8b93320f1a6
jfloresvaliente <jorgefloresvaliente@gmail.com>
2018-09-28 17:17:35 -0500

    new_rutine

--- single_country_in_Rmaps.R
+++ single_country_in_Rmaps.R
@@ -9,8 +9,20 @@ gg <- gg + geom_map(data=coast_map, map=coast_map,
                     aes(x=long, y=lat, map_id=region),
                     fill="white", color="black")
 gg <- gg + geom_map(data=data.frame(region="Peru"), map=coast_map,
-                    aes(map_id=region), fill="steelblue")
+                    aes(map_id=region), fill="grey60")
 gg <- gg + xlim(-90,-70) + ylim(-20,-2)
 gg <- gg + coord_map()
 gg <- gg + theme_bw()
 gg
+
+
+# Segunda forma
+library(maps)  
+library(mapdata)
+
+peru <- map("worldHires", regions="Peru", plot=FALSE, fill=TRUE)
+
+x11()
+map("worldHires", xlim=c(-90,-70), ylim=c(-20,0))
+map(peru, col="grey60", fill=TRUE, add=TRUE)
+box(lwd = 3)
