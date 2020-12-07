c118e6b6be779e115ab71de7cc7c82bc719ee6d3
Jorge Flores Valiente <jorgefloresvaliente@gmail.com>
2020-12-07 09:37:18 +0000

    vuelos

---
+++ dates_vuelos.R
@@ -0,0 +1,11 @@
+outDakar <- as.Date('2020-01-11')
+France   <- seq(from = outDakar, length.out = 120, by = 'day')
+outFranc <- France[length(France)]
+Peru     <- seq(from = outFranc, length.out = 90, by = 'day')
+outPeru  <- Peru[length(Peru)]
+
+c(outDakar, outFranc, outPeru)
+
+
+
+
