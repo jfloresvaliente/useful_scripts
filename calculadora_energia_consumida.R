66a3416c8314435a72292afc54991bc5afb11c1d
Jorge Flores Valiente <jorgefloresvaliente@gmail.com>
2024-01-11 16:37:41 -0500

    calculadora_consumo

---
+++ calculadora_energia_consumida.R
@@ -0,0 +1,22 @@
+# Calcular consumo de energia Alita
+library(lubridate)
+
+# Diferencia en lectura de medidor
+lec1 <- 11017.02 # Lectura inicial de medidor 
+lec2 <- 11017.27 # Lectura final de medidor
+lec_diff <- lec2 - lec1
+
+# Horas transcurridas
+hora1 <- ymd_hms('2024/01/09 21:05:00') # Hora de lectura inicial de medidor
+hora2 <- ymd_hms('2024/01/10 03:37:00') # Hora de lectura final de medidor
+hrs <- difftime(hora2, hora1, units = 'hours')
+hrs <- round(hrs[[1]], 2)
+
+# Calcular Kw/h
+Kwh <- lec_diff/hrs
+
+# Consumo mensual
+Kwm <- Kwh * 30 * 24
+
+print(paste('Kw/h   : ', round(Kwh, 3)))
+print(paste('Kw/mes : ', round(Kwm, 3)))
